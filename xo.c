0dded6cb017d36a57b0c300c9514227115150abf
hemalatha <avhemavinoth@gmail.com>
2016-07-15 15:09:42 +0530

    Update and rename xOO.C to xo.c

---
+++ xo.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+	void main() {
+	  char text[100];
+	  printf("Please enter strings:");
+
+	   gets(text);
+
+	  while (strlen(text) >= 1){
+
+
+
+
+
+Printf("The string is : %s\n",text);
+
+
+
+
+
+
+
+	     gets(text);
+
+
+
+	  }
+
+
+
+	}
